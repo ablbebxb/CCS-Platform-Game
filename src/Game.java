c159d6946de1d76a74865846ca483da33db322fe
Alex Johnson <johnsonalex1@me.com>
2012-09-28 23:36:28 -0400

    Finished up the architecture

--- src/Game.java
+++ src/Game.java
@@ -2,7 +2,64 @@
  * This is the Game class.  This class runs games.
  * 
  */
+import javax.swing.JFrame;
+import java.awt.event.KeyEvent;
 
-public class Game {
+public class Game extends JFrame{
 	
+	//dunno what this is, but JFrame classes complain when they dont have one =/
+	static final long serialVersionUID = 0;
+	
+	//static because it is a constant number, also indicated by the final keyword
+	/**
+	 * The x displacement of the terrain relative to the lefthand side of the screen.
+	 */
+	static final private int pos = 10;
+	
+	/**
+	 * The player in the game
+	 */
+	public Player player;
+	
+	/**
+	 * The terrain in the game
+	 */
+	public Terrain terrain;
+	
+	/**
+	 * Displays the terrain and the player in their proper positions
+	 */
+	public void display()
+	{
+		
+	}
+	
+	/**
+	 * Overrides a superclass implementation of this method, called every time
+	 * a user presses a key
+	 */
+	//the "@Override thing below is called an annotation, these can give the compiler directives so that the compiled program
+	//runs as you want it to, in certain cases, or simply runs faster in other cases
+	@Override
+	protected void processKeyEvent(KeyEvent e)
+	{
+		
+	}
+	
+	/**
+	 * Scrolls the screen to the side the given amount and generates the given amout of terrain
+	 * @param i how far to scroll the screen in pixels
+	 */
+	public void scrollScreen(int i)
+	{
+		
+	}
+	
+	/**
+	 * The game loop (runs one frame)
+	 */
+	public void gameLoop()
+	{
+		
+	}
 }
--- src/Player.java
+++ src/Player.java
@@ -4,7 +4,48 @@
  * The player class contains information about the position, movement, and appearance of a player in-game.
  */
 
+import java.awt.Image;
 
 public class Player {
-
+	private int x;//player's x position on the screen
+	private int y;//player's y position on the screen
+	
+	private int velocityY;//The change in Y to be excecuted every movement.
+	
+	private Image img;//The image used to represent the player
+	
+	/**
+	 * Sets the players vertical velocity
+	 * @param i the player's new vertical velocity
+	 */
+	public void SetVelocityY(int i)
+	{
+		
+	}
+	
+	/**
+	 * Modifies the player's current x position by the given amount
+	 * @param i The amount of pixels to modify the player's x position by
+	 */
+	public void MoveX(int i)
+	{
+		
+	}
+	
+	/**
+	 * Updates the player's current position based on its velocity
+	 */
+	public void update()
+	{
+		
+	}
+	
+	/**
+	 * Loads the image to use as the player's image
+	 * @param url The url of the image to load
+	 */
+	public void LoadImage(String url)
+	{
+		
+	}
 }
